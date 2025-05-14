f9318a71ef3b663cc55dbfc1daf285f9dc0b45db
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-05-14 22:46:15 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ intents = discord.Intents.default()
 intents.guilds = True
 intents.members = True
 
-bot = commands.Bot(intents=intents)
+bot = commands.Bot(command_prefix='', intents=intents)
 
 
 def ensure_json_files():
